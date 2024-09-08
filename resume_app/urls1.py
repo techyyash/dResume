4ed363839a77ebdd61c65d315cdb069f5478f070
yash <techyyash41@gmail.com>
2024-09-08 19:18:35 +0530

    -mgit upload

--- resume_app/urls1.py
+++ resume_app/urls1.py
@@ -18,15 +18,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-#from django.conf.urls import url
+from django.conf.urls import url
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace="main")),
-    path(r'^media/(?P<path>.*)$', serve,
+    url(r'^media/(?P<path>.*)$', serve,
         {'document_root': settings.MEDIA_ROOT}),
-    path(r'^static/(?P<path>.*)$', serve,
+    url(r'^static/(?P<path>.*)$', serve,
         {'document_root': settings.STATIC_ROOT}),
 ]
 
